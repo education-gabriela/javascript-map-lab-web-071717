a71fa11687775d768eb6ba9f6de7aa58d2aab1f2
gabidavila <gabidavila@gmail.com>
2017-08-29 17:53:52 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+let sortedRobots = robots.map(element => {
+  let alliance = 'autobot';
+
+  if (knownDecepticons.includes(element.name)) {
+    alliance = 'decepticon';
+  }
+  return Object.assign({}, element, {alliance: alliance});
+})
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +30,8 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+let coloredZebraStripes = zebraStripes.map((element, index) => {
+  let stripe = index % 2 === 0 ? 'black' : 'white';
+  return Object.assign({}, element, {color: stripe})
+})
